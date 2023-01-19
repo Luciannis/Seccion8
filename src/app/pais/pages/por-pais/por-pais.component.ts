776c40c9afbaf466dc35043ebc3cb3c2ccc3fdc6
Francisco Riveros <francisco@Franciscos-MacBook-Air.local>
2023-01-18 21:58:41 -0300

    termino del modulo 9

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,17 +5,26 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  styles:[
+    `
+    li{
+      cursos:pointer;
+    }
+  `]
 })
 export class PorPaisComponent  {
   
   termino:string = "";
   error:boolean = false;
-  paises: Country[] = []
+  paises: Country[] = [];
+
+  paisesSugeridos:Country[]=[];
+  mostrarSugerencias:boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino:string){
+    this.mostrarSugerencias = false;
     this.error = false;
     this.termino = termino;
 
@@ -33,6 +42,17 @@ export class PorPaisComponent  {
   }
   sugerencias(termino:string){
     this.error = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais(termino)
+    .subscribe(paises => this.paisesSugeridos = paises.splice(0,5),
+    (err) => this.paisesSugeridos = []);
+  }
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
+
   }
 
 }
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styles: [`
+  button{
+    margin-right:5px;
+  }`]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones:string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU','AL','ASEAN','CAIS','CEFTA','NAFTA','SAARC'];
+  regionActiva:string='';
+  paises:Country[] = [];
+  constructor(private paisService:PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  activarRegion(region:string){
+    if(region === this.regionActiva){return;}
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion(region)
+    .subscribe(paises => this.paises = paises);
+  }
+
+  getClaseCss(region:string):string{
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
   }
 
 }
